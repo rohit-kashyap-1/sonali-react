c848642409c13634709cba253f3835ef35d389e3
rohit-kashyap-1 <r23.tgc@gmail.com>
2024-05-15 16:48:21 +0530

    sonali's account created

--- src/App.js
+++ src/App.js
@@ -1,18 +1,73 @@
 import logo from './logo.svg';
 import './App.css';
-import Link from './Link';
+
+import dp from './images/dp.jpeg'
+
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+
+import About from './components/About';
+import Skills from './components/Skills';
+import Qualifications from './components/Qualifications'
+import Experience from './components/Experience'
+import Contact from './components/Contact'
+
 function App() {
   return (
-    <div className="App">
-      <nav>
-        <Link title="React Home" url="https://react.dev/url" id="1" />
-        <Link title="Documentation" url="https://react.dev/docs" id="2" />
-        <Link title="Pricing" url="https://react.dev/pricing" id="3" />
-       
-      </nav>
-    </div>
+    <BrowserRouter>
+      
+        <div className="App">
+          <div className='container py-4'>
+            <div className='row'>
+              <div className='col-md-3'>
+                <div className='border p-2  rounded'>
+                  <div className='p-4 text-center p-4'>
+                    <img src={dp} className='img-fluid rounded-circle mb-2 ' />
+                    <h4 className='mb-0'>Rohit Singh</h4>
+                    <h6 className='fw-light'>Full Stack PHP/Python Developer</h6>
+                  </div>
+
+                  
+                  <div className='list-group px-2 mb-2'>
+                    <div className='list-group-item active'>
+                      <Link to={'/about'} className='nav-link' >About Me</Link>
+                    </div>
+                    <div className='list-group-item'>
+                    <Link to={'/skills'} className='nav-link' >Skills</Link>
+                    </div>
+                    <div className='list-group-item'>
+                      <Link to={'/qualifications'} className='nav-link'>Qualifications</Link>
+                    </div>
+                    <div className='list-group-item'>
+                      <Link to={'/work-experience'} className='nav-link'>Work Experience</Link>
+                    </div>
+                    <div className='list-group-item'>
+                      <Link to={'/contact'} className='nav-link'>Contact</Link>
+                    </div>
+                  </div>
+                  
+
+                </div>
+              </div>
+              <div className='col-md-9'>
+                <div className='p-4 rounded border'>
+                  <Routes>
+                    <Route path='/' element={<About />} />
+                    <Route path='/about' element={<About />} />
+                    <Route path='/skills' element={<Skills />} />
+                    <Route path='/qualifications' element={<Qualifications />} />
+                    <Route path='/work-experience' element={<Experience />} />
+                    <Route path='/contact' element={<Contact />} />
+                  </Routes>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      
+    </BrowserRouter>
   );
 }
+
 
 
 export default App;
